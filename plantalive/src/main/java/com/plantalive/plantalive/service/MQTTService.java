f57c3ed1f27320415dd43afcb8edfdbf89f3a62d
Nico <nicorahm99@gmx.de>
2021-03-03 17:43:19 +0100

    added recieve MQTT message logic

--- plantalive/src/main/java/com/plantalive/plantalive/service/MQTTService.java
+++ plantalive/src/main/java/com/plantalive/plantalive/service/MQTTService.java
@@ -1,5 +1,7 @@
 package com.plantalive.plantalive.service;
 
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.springframework.stereotype.Service;
@@ -11,5 +13,20 @@ public interface MQTTService {
     default MqttMessage buildMqttMessage(String message) {
         byte[] payload = message.getBytes();
         return new MqttMessage(payload);
+    }
+
+    void subscribeTopic(String topicToBeSubscribed) throws MqttException;
+
+    default IMqttClient connectClient(IMqttClient client) throws MqttException {
+        client.connect(getMqttConnectOptions());
+        return client;
+    }
+
+    default MqttConnectOptions getMqttConnectOptions() {
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setAutomaticReconnect(true);
+        options.setCleanSession(true);
+        options.setConnectionTimeout(10);
+        return options;
     }
 }
--- plantalive/src/main/java/com/plantalive/plantalive/service/MQTTServiceImpl.java
+++ plantalive/src/main/java/com/plantalive/plantalive/service/MQTTServiceImpl.java
@@ -4,17 +4,15 @@ import com.plantalive.plantalive.config.MqttProperties;
 import com.plantalive.plantalive.config.MqttQOS;
 import com.plantalive.plantalive.exceptions.MqttClientNotConnectedException;
 import org.eclipse.paho.client.mqttv3.*;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MQTTServiceImpl implements MQTTService {
     private final IMqttClient mqttClient;
 
     public MQTTServiceImpl(MqttProperties mqttProperties) throws MqttException {
-        this.mqttClient = new MqttClient(mqttProperties.getBrokerUrl(),mqttProperties.getPublisherId());
-        MqttConnectOptions options = new MqttConnectOptions();
-        options.setAutomaticReconnect(true);
-        options.setCleanSession(true);
-        options.setConnectionTimeout(10);
-        this.mqttClient.connect(options);
+        IMqttClient clientToBeConnected = new MqttClient(mqttProperties.getBrokerUrl(),mqttProperties.getPublisherId());
+        this.mqttClient = connectClient(clientToBeConnected);
     }
 
     @Override
@@ -24,6 +22,20 @@ public class MQTTServiceImpl implements MQTTService {
         mqttMessage.setQos(MqttQOS.EXACTLY_ONCE);
         mqttMessage.setRetained(true);
         mqttClient.publish(topic,mqttMessage);
+    }
+
+    @Override
+    public void subscribeTopic(String topicToBeSubscribed) throws MqttException {
+        mqttClient.subscribe(topicToBeSubscribed, (topic, msg) -> {
+            System.out.println("#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#");
+            System.out.println("#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#");
+            System.out.println("#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#");
+            System.out.print("Received Message: ");
+            System.out.println(msg + " from Topic " + topic);
+            System.out.println("#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#");
+            System.out.println("#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#");
+            System.out.println("#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#");
+        });
     }
 
 }
